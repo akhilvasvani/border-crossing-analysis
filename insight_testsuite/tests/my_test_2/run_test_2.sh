7418748428036300d169deac8b9001a8d5735fac
Akhil V <sonofhotmale@gmail.com>
2019-08-23 17:55:05 -0400

    added in the separate bash files to run each unit test case

--- insight_testsuite/tests/my_test_2/run_test_2.sh
+++ insight_testsuite/tests/my_test_2/run_test_2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #
 # Use this shell script to compile (if necessary) your code and then execute it. Belw is an example of what might be found in this file if your program was written in Python 3.7
-python3 src/border_crossing_statistics.py --input input/Border_Crossing_Entry_Data.csv --output output/report.csv
+python3 src/border_crossing_statistics.py --input input/Border_Crossing_Entry_Data_test_same_repeat.csv --output output/report_test_same_repeat.csv
 # python3.7 ./src/border_analytics.py ./input/Border_Crossing_Entry_data.csv ./output/report.csv
